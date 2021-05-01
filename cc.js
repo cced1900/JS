88cbddd7212cd3a1d82af8b8775d996dd0404ae6
cced1900 <cced4400@gmail.com>
2021-05-01 13:20:57 +0800

    Update cc.js

--- cc.js
+++ cc.js
@@ -22,6 +22,6 @@ Unlocks by Cuttlefish 公众号：墨鱼手记
 // @originalValue     字段key对应的原始value
 // @replaceValue      字段key替换之后的value
 // @/g                正则匹配时匹配多个key键值
-// ==/KeyExplain== 虫虫钢琴
+// ==/KeyExplain== 虫虫钢琴,收费谱子可以通过PDF打印获取 ，直接点下载原谱无效
 var body = $response.body.replace(/is_vip":"0"/g, 'is_vip":"1"').replace(/vip_expire_date":""/g, 'vip_expire_date":"20211111"').replace(/has_buy":"0"/g, 'has_buy":"1"');
 $done({ body });
