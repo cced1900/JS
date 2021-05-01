4af75ee95b939fee18e287fb24488e9731f1cef7
cced1900 <cced4400@gmail.com>
2021-05-01 12:29:34 +0800

    Update cx.js

--- cx.js
+++ cx.js
@@ -1,10 +1,9 @@
 //https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/cxzk.js
-/*
-Unlocks by Cuttlefish 公众号：墨鱼手记
-*/
-var body = $response.body.replace(/\u0069\u0073\u0066\u0072\u0065\u0065\u0022\u003a\u0030/g, '\u0069\u0073\u0066\u0072\u0065\u0065\u0022\u003a\u0031');
-$done({ body });
-
+//*
+//Unlocks by Cuttlefish 公众号：墨鱼手记
+//*/
+//var body = $response.body.replace(/\u0069\u0073\u0066\u0072\u0065\u0065\u0022\u003a\u0030/g, '\u0069\u0073\u0066\u0072\u0065\u0065\u0022\u003a\u0031');
+//$done({ body });
 //https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/sample.js
 // ==UserScript==
 // @Name              网球规则[翻译]成Surge、QuantumultX、Loon脚本
@@ -14,7 +13,6 @@ $done({ body });
 // @UpdateTime        20200910
 // @ScriptURL         https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/sample.js
 // ==/UserScript==
-
 // ==KeyExplain==
 // @Function          text.replace(regexp/substr,replacement)
 // @key1              待替换的关键字段1
@@ -26,6 +24,6 @@ $done({ body });
 // @replaceValue      字段key替换之后的value
 // @/g                正则匹配时匹配多个key键值
 // ==/KeyExplain==
-
-var body = $response.body.replace(/"var isPay = document"/g, '"var isPay = 1;  #"').replace(/"var articleProperties"/g, '"var articleProperties2"')
+var body = $response.body.replace(/isPay = document/g, 'isPay = 1;  #').replace(/articleProperties/g, 'articleProperties2');
+//var body = $response.body.replace(/isPay = document/g, 'isPay = 1;  #')
 $done({ body });
